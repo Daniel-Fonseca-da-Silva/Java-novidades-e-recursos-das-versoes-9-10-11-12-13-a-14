e5f25b0713629e24ef4b0986d51ed8576fbb8dbf
Daniel Fonseca da Silva <developer-web@programmer.net>
2022-10-20 20:49:09 -0300

    feat: Fabrica para Collections

--- 1755-aula1-novidades-java-primeira_aula/src/br/com/alura/factorymethod/Teste.java
+++ 1755-aula1-novidades-java-primeira_aula/src/br/com/alura/factorymethod/Teste.java
@@ -1,18 +1,29 @@
 package br.com.alura.factorymethod;
 
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Teste {
 
+	// Novidade do java 9 - Factory Method para Collection
 	public static void main(String[] args) {
+		List<String> nomes = List.of("primeiroNome", "segundoNome");
+		Set.of("terceiroNome");
+		Map.of("nome", "Daniel");
+//		nomes.add("Erro");
+		nomes.forEach(nome -> {
+			System.out.println(nome);
+		});
+		
+		/*
 		ArrayList<String> nomes = new ArrayList<>();
 		nomes.add("primeiroNome");
 		nomes.add("segundoNome");
 		nomes.add("terceiroNome");
 		List<String> nomesImutavel = Collections.unmodifiableList(nomes);
 		System.out.println(nomesImutavel);
+		*/
 	}
 }
