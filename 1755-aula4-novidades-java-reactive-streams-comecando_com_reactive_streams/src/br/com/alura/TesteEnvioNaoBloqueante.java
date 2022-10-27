3d7c1da75e014e7f9a032544cda0a993dd6cee93
Daniel Fonseca da Silva <developer-web@programmer.net>
2022-10-27 00:00:00 -0300

    Trabalhando com diferentes threads

--- 1755-aula4-novidades-java-reactive-streams-comecando_com_reactive_streams/src/br/com/alura/TesteEnvioNaoBloqueante.java
+++ 1755-aula4-novidades-java-reactive-streams-comecando_com_reactive_streams/src/br/com/alura/TesteEnvioNaoBloqueante.java
@@ -2,6 +2,8 @@ package br.com.alura;
 
 import java.time.LocalDate;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.SubmissionPublisher;
 
 import br.com.alura.model.NotaFiscal;
@@ -10,10 +12,17 @@ import br.com.alura.wsclient.NotaFiscalWSClient;
 public class TesteEnvioNaoBloqueante {
 
 	public static void main(String[] args) {
+		ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(2);
 		NotaFiscal primeiraNotaFiscal = new NotaFiscal("Joao", 39.99, LocalDate.now());
-		SubmissionPublisher<NotaFiscal> publisher = new SubmissionPublisher<>();
+		SubmissionPublisher<NotaFiscal> publisher = new SubmissionPublisher<>(newFixedThreadPool, 1);
 		NotaFiscalWSClient nfwsc = new NotaFiscalWSClient();
 		publisher.consume(nfwsc::enviar);
+		publisher.consume(data -> {
+			System.out.println("Outra thread: " + Thread.currentThread().getName());
+		});
+		publisher.consume(data -> {
+			System.out.println("Mais outra thread: " + Thread.currentThread().getName());
+		});
 		publisher.submit(primeiraNotaFiscal);
 		System.out.println("Voce ira receber a nota fiscal no seu e-mail");
 		Scanner scan = new Scanner(System.in);
