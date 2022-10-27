aea9d5b56fd77c308d10f0e2dd569375b7366cda
Daniel Fonseca da Silva <developer-web@programmer.net>
2022-10-26 23:35:50 -0300

    Conhecendo fluxos reativos

---
+++ 1755-aula4-novidades-java-reactive-streams-comecando_com_reactive_streams/src/br/com/alura/TesteEnvioNaoBloqueante.java
@@ -0,0 +1,24 @@
+package br.com.alura;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+import java.util.concurrent.SubmissionPublisher;
+
+import br.com.alura.model.NotaFiscal;
+import br.com.alura.wsclient.NotaFiscalWSClient;
+
+public class TesteEnvioNaoBloqueante {
+
+	public static void main(String[] args) {
+		NotaFiscal primeiraNotaFiscal = new NotaFiscal("Joao", 39.99, LocalDate.now());
+		SubmissionPublisher<NotaFiscal> publisher = new SubmissionPublisher<>();
+		NotaFiscalWSClient nfwsc = new NotaFiscalWSClient();
+		publisher.consume(nfwsc::enviar);
+		publisher.submit(primeiraNotaFiscal);
+		System.out.println("Voce ira receber a nota fiscal no seu e-mail");
+		Scanner scan = new Scanner(System.in);
+		scan.nextLine();
+		scan.close();
+		publisher.close();
+	}
+}
