63eaaec9c3818d778a0d80d98df5ce254ac1bace
Daniel Fonseca da Silva <developer-web@programmer.net>
2022-10-26 22:24:43 -0300

    Requisição assíncrona com HTTP/2 Client API

--- 1755-aula3-novidades-java-http2-teste_requisicao_http2/src/br/com/alura/TesteRequestHttp2.java
+++ 1755-aula3-novidades-java-http2-teste_requisicao_http2/src/br/com/alura/TesteRequestHttp2.java
@@ -14,12 +14,12 @@ public class TesteRequestHttp2 {
 	public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
 
 		HttpClient httpClient = HttpClient.newHttpClient();
-		HttpRequest httpRequest = HttpRequest.newBuilder(new URI("https://www.casadocodigo.com.br"))
-				.GET().build();
-		HttpResponse<String> response = httpClient.send(httpRequest, BodyHandlers.ofString());
-		
-		System.out.println(response.statusCode());
-//		System.out.println(response.body());
-		System.out.println(response.version());
+		HttpRequest httpRequest = HttpRequest.newBuilder(new URI("https://www.casadocodigo.com.br")).GET().build();
+		HttpResponse<String> resp = httpClient.send(httpRequest, BodyHandlers.ofString());
+
+		System.out.println(resp.statusCode());
+		System.out.println(resp.body());
+		System.out.println(resp.version());
+
 	}
 }
